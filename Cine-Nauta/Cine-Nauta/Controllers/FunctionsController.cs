22ed3528174da2e979b7dd7f895067da70f8fa8d
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-10-24 17:17:59 -0500

    Create ActionCreate at Function

--- Cine-Nauta/Cine-Nauta/Controllers/FunctionsController.cs
+++ Cine-Nauta/Cine-Nauta/Controllers/FunctionsController.cs
@@ -1,4 +1,7 @@
 ﻿using Cine_Nauta.DAL;
+using Cine_Nauta.DAL.Entities;
+using Cine_Nauta.Helpers;
+using Cine_Nauta.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,12 +11,13 @@ namespace Cine_Nauta.Controllers
     {
 
         private readonly DataBaseContext _context;
+        private readonly IDropDownListHelper _dropDownListHelper;
 
-        public FunctionsController(DataBaseContext context)
+        public FunctionsController(DataBaseContext context, IDropDownListHelper dropDownListHelper)
         {
             _context = context;
+            _dropDownListHelper = dropDownListHelper;
         }
-
 
         public async Task<IActionResult> Index()
         {
@@ -23,5 +27,73 @@ namespace Cine_Nauta.Controllers
                  .Include(c => c.Room)
                 .ToListAsync());
         }
+
+
+
+        public async Task<IActionResult> Create()
+        {
+
+            AddFunctionViewModel addFunctionViewModel = new()
+            {
+                Movies = await _dropDownListHelper.GetDDLMoviesAsync(),
+                Rooms = await _dropDownListHelper.GetDDLRoomsAsync(),
+            };
+
+            return View(addFunctionViewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AddFunctionViewModel addFunctionViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+
+                    Function function = new()
+                    {
+                        Price= addFunctionViewModel.Price,
+                        CreatedDate = DateTime.Now,
+                        Movie = await _context.Movies.FindAsync(addFunctionViewModel.MovieId),
+                        Room = await _context.Rooms.FindAsync(addFunctionViewModel.RoomId),
+
+
+                    };
+
+
+
+
+
+                    _context.Add(function);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una función con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+
+            addFunctionViewModel.Movies = await _dropDownListHelper.GetDDLMoviesAsync();
+            addFunctionViewModel.Rooms = await _dropDownListHelper.GetDDLRoomsAsync();
+            return View(addFunctionViewModel);
+        }
+
+
     }
 }
---
+++ Cine-Nauta/Cine-Nauta/Models/AddFunctionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Cine_Nauta.DAL.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.Models
+{
+    public class AddFunctionViewModel : EntityCine
+    {
+        [Display(Name = "Fecha Función")]
+        public DateTime FunctionDate { get; set; }
+
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+
+
+        [Display(Name = "Pelicula")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int MovieId { get; set; }
+        public IEnumerable<SelectListItem> Movies { get; set; }
+
+        [Display(Name = "Sala")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int RoomId { get; set; }
+
+        public IEnumerable<SelectListItem> Rooms { get; set; }
+    }
+}
