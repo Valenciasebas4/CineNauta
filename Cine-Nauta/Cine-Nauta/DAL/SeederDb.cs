f32fa01c5da3bc384a3e403baad3d1e531de6601
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-10-24 09:39:45 -0500

    Create Users at class SeederDb

--- Cine-Nauta/Cine-Nauta/DAL/SeederDb.cs
+++ Cine-Nauta/Cine-Nauta/DAL/SeederDb.cs
@@ -1,28 +1,70 @@
 ﻿using Cine_Nauta.DAL.Entities;
+using Cine_Nauta.Emun;
+using Cine_Nauta.Helpers;
 
 namespace Cine_Nauta.DAL
 {
     public class SeederDb
     {
         private readonly DataBaseContext _context;
+        private readonly IUserHelper _userHelper;
 
 
-        public SeederDb(DataBaseContext context)
+        public SeederDb(DataBaseContext context, IUserHelper userHelper)
         {
             _context = context;
-            
+            _userHelper = userHelper;
+           
         }
 
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             await PopulateCountriesStatesCitiesAsync();
-            //await PopulateRolesAsync();
+            await PopulateRolesAsync();
+            await PopulateUserAsync("Sebastian", "Londoño", "sebas@yopmail.com", "3142393101", "Barbosa", "1035234145",  UserType.Admin);
+            await PopulateUserAsync("Jessica", "Gomez", "jess@yopmail.com", "3188955943", "Barbosa", "1035232261", UserType.User);
 
 
             await _context.SaveChangesAsync();
 
         }
+
+
+        private async Task PopulateRolesAsync()
+        {
+            await _userHelper.AddRoleAsync(UserType.Admin.ToString());
+            await _userHelper.AddRoleAsync(UserType.User.ToString());
+        }
+
+
+        private async Task PopulateUserAsync(string firstName, string lastName, string email, string phone, string address, string document,  UserType userType)
+        {
+            User user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                
+
+                user = new User
+                {
+                    CreatedDate = DateTime.Now,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    Document = document,
+                    City = _context.Cities.FirstOrDefault(),
+                    UserType = userType,
+                    
+                };
+
+                await _userHelper.AddUserAsync(user, "123456"); //se establece contraseña para el usuario
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());//agrega el usuario con el rol
+            }
+        }
+
 
         private async Task PopulateCountriesStatesCitiesAsync()
         {
@@ -113,5 +155,8 @@ namespace Cine_Nauta.DAL
                });
             }
         }
+
+
+
     }
 }
